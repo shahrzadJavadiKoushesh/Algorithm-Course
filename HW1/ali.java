0adf409366470c3cee089422f67f1eca0baf7c6b
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-02-25 22:23:53 +0330

    add ali practice

---
+++ HW1/ali.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+//
+public class ali {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        while (n > 0) {
+            int num = scanner.nextInt();
+            if (num >= 0){
+                numbers.add(num);
+            }
+            switch (num) {
+                case -1:
+                    removeFirstEven(numbers);
+                    break;
+                case -2:
+                    removeFirstOdd(numbers);
+                    break;
+                case -3:
+                    removeLastEven(numbers);
+                    break;
+                case -4:
+                    removeLastOdd(numbers);
+                    break;
+            }
+            n--;
+        }
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+
+        }
+    }
+
+    private static void removeFirstEven(ArrayList<Integer> array) {
+//        System.out.println("removeFirstEven");
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) % 2 == 0) {
+//                System.out.println("removing" + array.get(i));
+                array.remove(i);
+                break;
+            }
+        }
+//        System.out.println(array);
+    }
+
+    private static void removeFirstOdd(ArrayList<Integer> array) {
+//        System.out.println("removeFirstOdd");
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) % 2 != 0) {
+//                System.out.println("removing" + array.get(i));
+                array.remove(i);
+                break;
+            }
+        }
+//        System.out.println(array);
+    }
+
+    private static void removeLastEven(ArrayList<Integer> array) {
+//        System.out.println("removeLastEven");
+        for (int i = array.size() - 1; i >= 0; i--) {
+            if (array.get(i) % 2 == 0) {
+//                System.out.println("removing" + array.get(i));
+                array.remove(i);
+                break;
+            }
+        }
+//        System.out.println(array);
+    }
+
+    private static void removeLastOdd(ArrayList<Integer> array) {
+//        System.out.println("removeLastOdd");
+        for (int i = array.size() - 1; i >= 0; i--) {
+            if (array.get(i) % 2 != 0) {
+//                System.out.println("removing" + array.get(i));
+                array.remove(i);
+                break;
+            }
+        }
+//        System.out.println(array);
+    }
+}
