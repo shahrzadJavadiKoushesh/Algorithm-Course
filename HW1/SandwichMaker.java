76450f1802293aae6113d32653a847bed1007ab1
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-02-28 12:16:34 +0330

    add sandwichmaker, runtime error and timelimit and wrong :(

--- HW1/SandwichMaker.java
+++ HW1/SandwichMaker.java
@@ -11,6 +11,7 @@ public class SandwichMaker {
         int maxLength = 0;
         String curMaxSub = "";
         String prevMaxSub;
+        boolean flagForDetectingOthers = true;
         ArrayList<Integer> maxes = new ArrayList<Integer>();
         int flag = 0;
         for (int i = 0; i < s.length(); i++) {
@@ -28,32 +29,74 @@ public class SandwichMaker {
                 }
                 if (bCount == mCount){
                     prevMaxSub = curMaxSub;
-                    if (flag + 1 == i){
+                    if (i == 0){
+                        curMaxSub = s.substring(i, j);
+                        System.out.println("FUUUCk");
+                        System.out.println("i " + i);
+                        System.out.println("j " + (j-1));
+                        System.out.println("cur: " + curMaxSub);
+                        System.out.println("prev: " + prevMaxSub);
+                        flag = j - 1;
+                        System.out.println("flag: " + flag);
+                        System.out.println("----");
+                        maxLength = curMaxSub.length();
+                    }
+                    else if (flag + 1 == i){
+//                      two valid substrings next to each other
+                        System.out.println("entered else if");
                         curMaxSub = s.substring(i, j);
                         curMaxSub = prevMaxSub + curMaxSub;
                         System.out.println("i " + i);
+                        flag = j - 1;
                         System.out.println("j " + (j-1));
                         System.out.println("cur: " + curMaxSub);
                         System.out.println("prev: " + prevMaxSub);
                         System.out.println("flag: " + flag);
                         System.out.println("----");
-                        flag = j - 1;
+                        maxLength = curMaxSub.length();
                     }else {
+                        System.out.println("entered else");
+                        System.out.println("i " + i);
+                        System.out.println(s.substring(0, i));
+                        if (s.substring(0, i).contains("b")){
+                            System.out.println(s.substring(0, i));
+                            System.out.println("Ssssssss");
+                            for (int k = flag + 1; k < i; k++) {
+                                System.out.println("k " + k);
+                                if (s.charAt(k) == 'm' || s.charAt(k) == 'b'){
+                                    flagForDetectingOthers = false;
+                                    System.out.println("break");
+                                    break;
+                                }
+                            }
+                        }
+
+//                      bsmppbssm -> to detect the pp
+                        if (s.substring(0, i).contains("b")){
+                            if (flagForDetectingOthers){
+                                curMaxSub = curMaxSub + s.substring(flag + 1, i);
+                                System.out.println("used flagForDetectingOthers");
+                                System.out.println(curMaxSub);
+                            }
+                        }
+
                         flag = j - 1;
-                        curMaxSub = s.substring(i, j);
+                        prevMaxSub = curMaxSub;
+                        curMaxSub = prevMaxSub + s.substring(i, j);
                         System.out.println("i " + i);
                         System.out.println("j " + (j-1));
                         System.out.println("cur: " + curMaxSub);
                         System.out.println("prev: " + prevMaxSub);
                         System.out.println("flag: " + flag);
                         System.out.println("----");
+                        maxLength = curMaxSub.length();
                     }
-                    maxLength = curMaxSub.length();
                     System.out.println("max len: " + maxLength);
                     maxes.add(maxLength);
                 }
             }
         }
+        System.out.println(maxes);
         if (!maxes.isEmpty()) {
             maxLength = Collections.max(maxes);
         }
