ef8de0a2c8524d1f803ba5f55aa5dd435fdf66f4
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-03-03 22:48:57 +0330

    add crab points

--- HW1/Crab.java
+++ HW1/Crab.java
@@ -1,65 +1,66 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Crab
-{
+public class Crab {
     static int findMaxLen(String str) {
 
-        Stack<Integer> stk = new Stack<>();
-
-        int result = 0;
-        int flag = 0;
-        int x = 0;
-        int lasti = 0;
-        for (int i = 0; i < str.length(); i++) {
-            System.out.println("i: " + i);
-            if (str.charAt(i) == 'b') {
-                if (stk.empty()) {
-                    stk.push(i - 1);
-                }
-                stk.push(i);
-                System.out.println("detected b");
-                System.out.println(stk);
+        int mCount = 0, bCount = 0, othersCount = 0, maxLen = 0, temp;
+        for (int i = str.indexOf('b'); i < str.lastIndexOf('m'); i++) {
+            if (str.charAt(i) == 'b'){
+                bCount++;
             }
-
-            else if (str.charAt(i) == 'm') {
-                System.out.println("detected m");
-                if(!stk.empty()) {
-                    flag = stk.peek();
-                    stk.pop();
-                    System.out.println("stack after popping: "  +stk);
+            else if (str.charAt(i) == 'm'){
+                mCount++;
+            }
+            else if ((mCount > 0 || bCount > 0) && (str.charAt(i) == 's' || str.charAt(i) == 'p' || str.charAt(i) == 't')) {
+                othersCount++;
+            }
+            if (bCount == mCount && str.charAt(i) == 'm'){
+                temp = mCount + bCount + othersCount;
+                if (temp > maxLen){
+                    maxLen = temp;
                 }
-                if (!stk.empty()) {
-                    System.out.println("flag: " + flag);
-                    if (lasti + 1 == flag){
-                        System.out.println("chasbide");
-                        x = i - flag + 1;
-                        result = result + x;
-                        System.out.println(result);
-                        System.out.println("goftam ke chasbide");
-                    }
-                    x = i - flag + 1;
-                    System.out.println("x: " + x);
-                    if (x >= result){
-                        lasti = i;
-                        result = x;
-                        System.out.println("last i changed to: " + lasti);
-                    }
-
-                    System.out.println("result: " + result);
-                    System.out.println("x: " + x);
-                }
-
+            }
+            if (mCount > bCount){
+                mCount = 0;
+                bCount = 0;
+                othersCount = 0;
             }
         }
+        bCount = 0;
+        mCount = 0;
+        othersCount = 0;
 
-        return result;
+        for (int i = str.lastIndexOf('m'); i >= str.indexOf('b') ; i--) {
+            if (str.charAt(i) == 'm'){
+                mCount++;
+            }
+            else if(str.charAt(i) == 'b'){
+                bCount++;
+            }
+            else if ((mCount > 0 || bCount > 0) && (str.charAt(i) == 's' || str.charAt(i) == 'p' || str.charAt(i) == 't')){
+                othersCount++;
+            }
+            if (mCount == bCount && str.charAt(i) == 'b'){
+                temp = mCount + bCount + othersCount;
+                if (temp > maxLen){
+                    maxLen = temp;
+                }
+            }
+            if (bCount > mCount){
+                mCount = 0;
+                bCount = 0;
+                othersCount = 0;
+            }
+        }
+        return maxLen;
+
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
         System.out.println(findMaxLen(input));
     }
 }
+
