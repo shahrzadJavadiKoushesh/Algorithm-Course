1e44788a1b7dd9fd0a8238bb538ba15b2209f716
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-03-09 17:53:18 +0330

    sit 80

--- HW1/sit.java
+++ HW1/sit.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class sit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();  // row
         int m = scanner.nextInt();  // column
@@ -23,15 +25,9 @@ public class sit {
                     pq.addAll(calculateDistance(seats[i], i));
                 }
             }
-//            System.out.println("queue at iteration " + (s + 1));
-//            for (SalonSeat seat : pq) {
-//                System.out.println(seat.row + " " + seat.col + " " + seat.distance);
-//            }
             SalonSeat seat = pq.poll();
-//            System.out.println("to poll");
             System.out.println(seat.row + " " + seat.col);
             seats[seat.row - 1] = seats[seat.row - 1].substring(0, seat.col - 1) + '#' + seats[seat.row - 1].substring(seat.col);
-//            System.out.println(seats[seat.row - 1]);
             modifiedIndice = seat.row - 1;
             int finalModifiedIndice = modifiedIndice;
             pq.removeIf(obj -> obj.row == finalModifiedIndice + 1);
@@ -43,46 +39,64 @@ public class sit {
         int rightDistance;
         int finalDistance;
 
-        char[] seatsArr = seats.toCharArray();
         PriorityQueue<SalonSeat> salonSeats = new PriorityQueue<>(new SeatComparator());
         PriorityQueue<SalonSeat> curRow = new PriorityQueue<>(new SeatComparator());
-        SalonSeat bestSeatOfRow;
 
-        for (int i = 0; i < seatsArr.length; i++) {
-            if (seatsArr[i] != '#') {
-                leftDistance = Integer.MAX_VALUE;
-                rightDistance = Integer.MAX_VALUE;
-                // right
-                if (seatsArr[i] == '.' && i < seatsArr.length - 1) {
-                    String fromHere = seats.substring(i);
-                    if (fromHere.indexOf('#') != -1) {
-                        rightDistance = (fromHere.substring(0, fromHere.indexOf('#'))).length();
+        int [] occupiedIndexes = IntStream.range(0, seats.length()).filter(i -> seats.charAt(i) == '#').toArray();
+
+        if (seats.contains(".")){
+            for (int i = 0; i < seats.length(); i++) {
+                if (seats.charAt(i) != '#') {
+                    int [] result = findElement(occupiedIndexes, i);
+                    if (result[0] == -1){
+                        leftDistance = Integer.MAX_VALUE;
+                    } else {
+                        leftDistance = i - result[0];
                     }
-                }
-                // left
-                if (seatsArr[i] == '.' && i > 0) {
-                    String fromHere = seats.substring(0, i + 1);
-                    if (fromHere.contains("#")) {
-                        leftDistance = fromHere.substring(fromHere.lastIndexOf('#'), i).length();
+                    if (result[1] == -1){
+                        rightDistance = Integer.MAX_VALUE;
+                    } else {
+                        rightDistance = result[1] - i;
                     }
+                    finalDistance = Integer.min(leftDistance, rightDistance);
+                    curRow.add(new SalonSeat(row + 1, i + 1, finalDistance));
                 }
-                finalDistance = Math.min(leftDistance, rightDistance);
-            } else {
-                finalDistance = 0;
             }
-            curRow.add(new SalonSeat(row + 1, i + 1, finalDistance));
+            salonSeats.add(curRow.poll());
         }
-//        System.out.println("array curRow:");
-//        for (SalonSeat salonSeat : curRow) {
-//            System.out.println(salonSeat.row + " " + salonSeat.col + " " + salonSeat.distance);
-//        }
-        bestSeatOfRow = curRow.poll();
-        salonSeats.add(bestSeatOfRow);
-//        System.out.println("salonSeats:");
-//        for (SalonSeat salonSeat : salonSeats) {
-//            System.out.println(salonSeat.row + " " + salonSeat.col + " " + salonSeat.distance);
-//        }
         return salonSeats;
+    }
+
+    public static int[] findElement(int [] occupiedIndexes, int index){
+        int leftIndex = -1;
+        int rightIndex = -1;
+        int low = 0;
+        int high = occupiedIndexes.length - 1;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            if (occupiedIndexes[mid] == index){
+                leftIndex = rightIndex = mid;
+                break;
+            } else if (occupiedIndexes[mid] < index) {
+                leftIndex = mid;
+                low = mid + 1;
+            } else {
+                rightIndex = mid;
+                high = mid - 1;
+            }
+        }
+
+        if (leftIndex == -1) {
+            leftIndex = high;
+        }
+        if (rightIndex == -1) {
+            rightIndex = low;
+        }
+
+        int leftElement = leftIndex >= 0 ? occupiedIndexes[leftIndex] : -1;
+        int rightElement = rightIndex < occupiedIndexes.length ? occupiedIndexes[rightIndex] : -1;
+
+        return new int[]{leftElement, rightElement};
     }
 
     static class SalonSeat {
